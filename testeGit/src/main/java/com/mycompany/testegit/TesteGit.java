31ef06413c4a26d7e0a307b1e310e64fcb8f499f
JVGouveia <gouveia.j.vitor@gmail.com>
2022-09-04 15:38:38 -0300

    teste Commit

--- testeGit/src/main/java/com/mycompany/testegit/TesteGit.java
+++ testeGit/src/main/java/com/mycompany/testegit/TesteGit.java
@@ -8,7 +8,5 @@ public class TesteGit {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
     }
 }
